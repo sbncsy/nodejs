8fab5d0dc48388e54198b4a08b6579fb1159f70c
sbncsy <sbncsy@gmail.com>
2017-01-14 09:37:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,8 @@ var restify = require('restify');
 var pg = require('pg');
 var util = require('util');
 var optimist = require('optimist').argv;
-// var AWS = require("aws-sdk");
-// AWS.config.loadFromPath('./.aws/credentials.json'); 
-
-// AWS.config.update({
-//   region: "us-west-2",
-//   endpoint: "http://localhost:8080"
-// });
-
+ 
+ 
 var Init = function(username, password, db) {
     var connection = util.format("postgres://%s:%s@localhost/%s", username, password, db);
     var server = restify.createServer({
@@ -19,7 +13,27 @@ var Init = function(username, password, db) {
     server.use(restify.acceptParser(server.acceptable));
     server.use(restify.queryParser());
     server.use(restify.bodyParser());
-
+ 
+    // Return full result from query
+    server.post('/query', function(req, res, next) {
+        console.log(req.params.query, connection);
+        pg.connect(connection, function(err, client, done) {
+            if (err) {
+                res.send({result:"null", type: "connection", error: err});
+            }
+ 
+            client.query(req.params.query, function(err, result) {
+                done();
+                if(err) {
+                    res.send({result:"null", type: "query", error:err});
+                }
+ 
+                res.send({id: result.rows[0].id, lat: result.rows[0].lat, lon: result.rows[0].lon, distance: result.rows[0].distance});
+            });
+        });
+        return next();
+    });
+ 
     server.post('/route', function(req, res, next) {
         // console.log(req.params.query, connection);
         const steps = [];
@@ -43,10 +57,10 @@ var Init = function(username, password, db) {
                   return res.status(500).json({success: false, data: err})
                 }
             });
-               
-            query.on('row', function(row) {
+                
+            query.on('row', (row) => {
                 steps.push({"geometry":row.the_geom,"maneuver":{"location":[Number(row.lat), Number(row.lon)]}});
-
+ 
             });
             const query_2 = client.query(sql_2, function(err, result) {
                 if(err) {
@@ -54,17 +68,49 @@ var Init = function(username, password, db) {
                   console.log(err);
                   return res.status(500).json({success: false, data: err})
                 }
-//                 geometry = result.rows[0].geometry;
+                geometry = result.rows[0].geometry;
             });
-
+ 
             // After all data is returned, close connection and return results
-            query_2.on('end', function() {
+            query_2.on('end', () => {
               done();
-              return res.send(JSON.stringify({"routes":[{geometry,"legs":[{"steps":steps}]}]}));
+              return res.json({"routes":[{geometry,"legs":[{"steps":steps}]}]});
             });
         });
     });
-
+ 
+ 
+    server.post('/route2', function(req, res, next) {
+        console.log(req.params.query, connection);
+        const steps = [];
+        // const lat = [];
+        // const lon = [];
+        pg.connect(connection, function(err, client, done) {
+            if(err) {
+              done();
+              console.log(err);
+              return res.status(500).json({success: false, data: err})
+            }
+                 
+            const query = client.query(req.params.nquery, function(err, result) {
+                if(err) {
+                  done();
+                  console.log(err);
+                  return res.status(500).json({success: false, data: err})
+                }
+            });
+                
+            query.on('row', (row) => {
+                steps.push({"geometry":row.the_geom,"maneuver":{"location":[Number(row.lat), Number(row.lon)]}});
+ 
+            });
+            // After all data is returned, close connection and return results
+            query.on('end', () => {
+              done();
+              return res.json({"routes":[{"legs":[{"steps":steps}]}]});
+            });
+        });
+    });
         // Return full result from query
     server.post('/nearnode', function(req, res, next) {
         console.log(req.params.query, connection);
@@ -72,39 +118,56 @@ var Init = function(username, password, db) {
             if (err) {
                 res.send({result:"null", type: "connection", error: err});
             }
-
+ 
             client.query(req.params.query, function(err, result) {
                 done();
                 if(err) {
                     res.send({result:"null", type: "query", error:err});
                 }
-
+ 
                 res.send({id: result.rows[0].id, lat: result.rows[0].lat, lon: result.rows[0].lon, distance: result.rows[0].distance});
             });
         });
         return next();
     });
-
-    
+ 
+    server.post('/nearnode2', function(req, res, next) {
+        console.log(req.params.query, connection);
+        pg.connect(connection, function(err, client, done) {
+            if (err) {
+                res.send({result:"null", type: "connection", error: err});
+            }
+ 
+            client.query(req.params.query, function(err, result) {
+                done();
+                if(err) {
+                    res.send({result:"null", type: "query", error:err});
+                }
+ 
+                res.send({id: result.rows[0].id, lat: result.rows[0].lat, lon: result.rows[0].lon, distance: result.rows[0].distance});
+            });
+        });
+        return next();
+    });
     server.listen(8080, function() {
         console.log('%s listening at %s', server.name, server.url);
     });
 };
-
+ 
 var name = "postgres";
 var password = "postgres";
 var db = "postgres";
-
+ 
 if(optimist.name !== undefined) {
     name = optimist.name;
 }
-
+ 
 if(optimist.password !== undefined) {
     password = optimist.password;
 }
-
+ 
 if(optimist.db !== undefined) {
     db = optimist.db;
 }
-
+ 
 Init(name,password,db);
